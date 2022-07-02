335ae87e5c107f0ed5804d2f1b27e121f07e3215
test <yusuke.1202.n@gmail.com>
2022-07-03 01:29:40 +0900

    [add]job一覧を取得するapiの実装

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Job;
 
 class JobController extends Controller
 {
-    //
+    public function index()
+     {
+         return Job::all();
+     }
 }
