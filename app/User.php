8cc6b7af658cefca1bbec79603c75f899a350ed1
test <yusuke.1202.n@gmail.com>
2022-07-04 00:04:49 +0900

    [update]変更可能カラムにself_introductionを追加

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'sei', 'mei', 'sei_kana', 'mei_kana', 'email', 'password', 'address', 'gender', 'age', 'self_introduction'
     ];
 
     /**
