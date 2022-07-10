f69848b17e3c5998a17c1c62e2e8e2ed5f2fe8d3
nakagawa <yusuke.1202.n@gmail.com>
2022-07-11 01:55:26 +0900

    [update]ヘッダーを上部に固定

--- resources/js/components/share/HeaderComponent.vue
+++ resources/js/components/share/HeaderComponent.vue
@@ -1,21 +1,21 @@
 <template>
-<header>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" v-if="user">
-  <h5 class="my-0 mr-md-auto font-weight-normal"><router-link v-bind:to="{name: 'user.list'}" class="text-dark">だれでもJob</router-link></h5>
-  <nav class="my-2 my-md-0 mr-md-3">
-    <router-link v-bind:to="{name: 'job.list'}" class="p-2 text-dark">依頼されている仕事一覧</router-link>
-    <router-link v-bind:to="{name: 'room.list'}" class="p-2 text-dark">メッセージ一覧</router-link>
-    <router-link v-bind:to="{name: 'mypage'}" class="p-2 text-dark">プロフィール</router-link>
-    <router-link v-bind:to="{name: 'room.list'}" class="p-2 text-dark">メッセージ一覧</router-link>
-  </nav>
-  <a class="btn btn-outline-primary" @click="logout">ログアウト</a>
-</div>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" v-else>
-  <h5 class="my-0 mr-md-auto font-weight-normal"><router-link v-bind:to="{name: 'user.list'}" class="text-dark">だれでもJob</router-link></h5>
-  <router-link v-bind:to="{name: 'signup'}" class="btn btn-outline-primary mr-2">新規登録</router-link>
-  <router-link v-bind:to="{name: 'login'}" class="btn btn-outline-primary">ログイン</router-link>
-</div>
-</header>
+    <header class="sticky-top">
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" v-if="user">
+            <h5 class="my-0 mr-md-auto font-weight-normal"><router-link v-bind:to="{name: 'user.list'}" class="text-dark">だれでもJob</router-link></h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+                <router-link v-bind:to="{name: 'job.list'}" class="p-2 text-dark">依頼されている仕事一覧</router-link>
+                <router-link v-bind:to="{name: 'room.list'}" class="p-2 text-dark">メッセージ一覧</router-link>
+                <router-link v-bind:to="{name: 'mypage'}" class="p-2 text-dark">プロフィール</router-link>
+                <router-link v-bind:to="{name: 'room.list'}" class="p-2 text-dark">メッセージ一覧</router-link>
+            </nav>
+            <a class="btn btn-outline-primary" @click="logout">ログアウト</a>
+        </div>
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" v-else>
+            <h5 class="my-0 mr-md-auto font-weight-normal"><router-link v-bind:to="{name: 'user.list'}" class="text-dark">だれでもJob</router-link></h5>
+            <router-link v-bind:to="{name: 'signup'}" class="btn btn-outline-primary mr-2">新規登録</router-link>
+            <router-link v-bind:to="{name: 'login'}" class="btn btn-outline-primary">ログイン</router-link>
+        </div>
+    </header>
 </template>
 
 <script>
