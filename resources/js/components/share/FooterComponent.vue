5572eb240189621bb9c5320dd4595d01d3bb2c8c
nakagawa <yusuke.1202.n@gmail.com>
2022-07-16 03:27:56 +0900

    [update]fotterの項目変更

--- resources/js/components/share/FooterComponent.vue
+++ resources/js/components/share/FooterComponent.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="container-fluid bg-dark mb-3">
-        <div class="container">
-            <nav class="navbar navbar-dark">
-                <div>
-                    <button class="btn btn-success">お問い合わせ</button>
-                    <button class="btn btn-success">プライバシーポリシー</button>
-                </div>
-            </nav>
-        </div>
-    </div>
+<nav class="navbar navbar-expand-sm navbar-dark bg-theme">
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="https://docs.google.com/forms/d/e/1FAIpQLScKbiCope3yvvCaZr327_5ecGSTwM1Awvk4laYTxG6XbAUeKg/viewform">お問い合わせ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">プライバシーポリシー</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 </template>
 
 <script>
